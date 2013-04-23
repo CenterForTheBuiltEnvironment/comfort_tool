7175bfd5393d9c5462230dc281a79409a770d36c
tyler.hoyt <tyler.hoyt@a2c09a3d-cacd-5e18-4c1e-d7bddea54d0f>
2013-04-23 18:22:57 +0000

    Fixed settings.py and added license casually

--- trunk/comforttool/settings.py
+++ trunk/comforttool/settings.py
@@ -4,7 +4,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Tyler Hoyt', 'tyler.hoyt@gmail.com'),
 )
 
 MANAGERS = ADMINS
@@ -59,7 +59,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/opt/comforttool/comforttool/static/'
+STATIC_ROOT = '/opt/cbe-comfort-tool/comforttool/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -70,7 +70,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/opt/comforttool/comforttool/ash55/static",
+    "/opt/cbe-comfort-tool/comforttool/ash55/static",
 )
 
 # List of finder classes that know how to find static files in
@@ -120,7 +120,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/opt/comforttool/comforttool/templates",
+    "/opt/cbe-comfort-tool/comforttool/templates",
 )
 
 INSTALLED_APPS = (
