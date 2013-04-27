d843a62db5373fe9e1f912a9cad9a2dd19a90f1f
tyler.hoyt <tyler.hoyt@a2c09a3d-cacd-5e18-4c1e-d7bddea54d0f>
2013-04-27 00:57:12 +0000

    Fixed insanely mangled file structure

--- trunk/comforttool/ash55/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('',
-
-    url(r'^$', 'comforttool.ash55.views.index')
-
-)
--- trunk/comforttool/ash55/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render_to_response
-from django.template import Context, loader
-from django.http import HttpResponse
-
-def index(request):
-    t = loader.get_template('index.html')
-    c = Context({})
-    return HttpResponse(t.render(c))
