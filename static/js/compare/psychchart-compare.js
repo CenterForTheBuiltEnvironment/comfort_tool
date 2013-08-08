dd9d8bcb736f17c16db7c6ca5cb8b57aa0634d5d
alberto.piccioli.87 <alberto.piccioli.87@a2c09a3d-cacd-5e18-4c1e-d7bddea54d0f>
2013-08-08 23:10:35 +0000

    drybulb to dry-bulb and addition of EN page to navbar

--- static/js/compare/psychchart-compare.js
+++ static/js/compare/psychchart-compare.js
@@ -279,14 +279,14 @@ var pc = new function() {
 
             d3.select("#db-axis-C")
                 .append("text")
-                .text("Drybulb Temperature [°C]")
+                .text("Dry-bulb Temperature [°C]")
                 .attr("class", "db-unit")
                 .attr("x", (pc.width / 2) - 1.9 * pc.margin)
                 .attr("y", pc.rbmargin / 1.3)
 
             d3.select("#db-axis-F")
                 .append("text")
-                .text("Drybulb Temperature [°F]")
+                .text("Dry-bulb Temperature [°F]")
                 .attr("class", "db-unit")
                 .attr("x", (pc.width / 2) - 1.9 * pc.margin)
                 .attr("y", pc.rbmargin / 1.3)
