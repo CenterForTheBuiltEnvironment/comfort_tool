cc33fddca8163ad9cb8b2e033eb404fdeb934e70
Tyler Hoyt <tyler.hoyt@gmail.com>
2017-06-25 16:37:28 -0700

    Adds wsgi.py

---
+++ wsgi.py
@@ -0,0 +1,7 @@
+from comfort import app
+
+#activate_this = '/src/comfort_tool/venv/bin/activate_this.py'
+#execfile(activate_this, dict(__file__=activate_this))
+
+if __name__ == "__main__":
+        application.run()
