521d3f13396b119f8be8be0bc46be7907dff7f29
FedericoTartarini <federicotartarini@gmail.com>
2019-07-09 10:42:30 +0800

    - Removed ignore files

--- contrib/test_comfort_models.py
+++ contrib/test_comfort_models.py
@@ -1,22 +1,25 @@
+import unittest
 import comfort_models
-import numpy as np
-import pandas as pd
+import csv
 
 
-def test_comfAdaptiveComfortASH55():
-    """ Test the SET function using the reference table from the ASHRAE 55 2017"""
+class TestComfortFunctions(unittest.TestCase):
 
-    assert comfort_models.comfAdaptiveComfortASH55(15, 15, 10, 0.1, True, 0)[4] == False
-    assert comfort_models.comfAdaptiveComfortASH55(24, 24, 20, 0.1, True, 0)[4] == True
+    def test_comfAdaptiveComfortASH55(self):
+        """ Test the SET function using the reference table from the ASHRAE 55 2017"""
 
+        self.assertFalse(comfort_models.comfAdaptiveComfortASH55(15, 15, 10, 0.1, True, 0)[4])
+        self.assertTrue(comfort_models.comfAdaptiveComfortASH55(24, 24, 20, 0.1, True, 0)[4])
 
-def test_comfPierceSET():
-    """ Test the SET function using the reference table from the ASHRAE 55 2017"""
+    def test_comfPierceSET(self):
+        """ Test the SET function using the reference table from the ASHRAE 55 2017"""
 
-    # import reference table
-    df_set = pd.read_csv('SET_test.csv')
-
-    # loop over the rows
-    for index, row in df_set.iterrows():
-        assert np.round(comfort_models.comfPierceSET(row['t'], row['mrt'], row['v'], row['rh'], row['met'], row['clo']), 1) == row['set']
-
+        # import reference table
+        with open('SET_test.csv') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            for row in csv_reader:
+                # convert elements to float
+                for el in row.keys():
+                    row[el] = float(row[el])
+                # assert that the SET calculated value matches the value from the ASHRAE standard
+                self.assertEqual(round(comfort_models.comfPierceSET(row['t'], row['mrt'], row['v'], row['rh'], row['met'], row['clo']), 1), row['set'])
