639191dc4f4286cdf6f66087bc3b129da84a286b
FedericoTartarini <federicotartarini@gmail.com>
2019-10-02 16:36:07 +0800

    - changed wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,13 @@
+# from comfort import app
+#
+# # activate_this = '/src/comfort_tool/venv/bin/activate_this.py'
+# # execfile(activate_this, dict(__file__=activate_this))
+#
+# if __name__ == "__main__":
+#     application.run()
+
+import os
 from comfort import app
 
-#activate_this = '/src/comfort_tool/venv/bin/activate_this.py'
-#execfile(activate_this, dict(__file__=activate_this))
-
-if __name__ == "__main__":
-        application.run()
+port = int(os.environ.get("PORT", 5000))
+app.run(debug=True, host='0.0.0.0', port=port)
