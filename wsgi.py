3a482f70bc5f1ebb27fdd74b61f9fbca49d3d0f8
Tyler Hoyt <thoyt@users.noreply.github.com>
2019-11-25 23:12:48 -0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -9,6 +9,6 @@
 import os
 from comfort import app
 
-+if __name__=="__main__":
-+    port = int(os.environ.get("PORT", 5000))
-+    app.run(host='0.0.0.0', port=port)
+if __name__=="__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
