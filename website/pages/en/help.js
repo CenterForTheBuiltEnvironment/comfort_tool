c813fa04b2fa9e6142474afd125e7ad7daa8c5e1
FedericoTartarini <federicotartarini@gmail.com>
2021-03-29 16:10:07 +0800

    feature: edited help page

--- website/pages/en/help.js
+++ website/pages/en/help.js
@@ -3,36 +3,12 @@ const React = require("react");
 const CompLibrary = require("../../core/CompLibrary.js");
 
 const Container = CompLibrary.Container;
-const GridBlock = CompLibrary.GridBlock;
 
-function Help(props) {
-  const { config: siteConfig, language = "" } = props;
-  const { baseUrl, docsUrl } = siteConfig;
-  const docsPart = `${docsUrl ? `${docsUrl}/` : ""}`;
-  const langPart = `${language ? `${language}/` : ""}`;
-  const docUrl = (doc) => `${baseUrl}${docsPart}${langPart}${doc}`;
-
-  const supportLinks = [
-    // {
-    //   content: `Learn more using the [documentation on this site.](${docUrl(
-    //     'ashrae55.html',
-    //   )})`,
-    //   title: 'Browse Docs',
-    // },
-    // {
-    //   content: 'Ask questions about the documentation and project',
-    //   title: 'Join the community',
-    // },
-    // {
-    //   content: "Find out what's new with this project",
-    //   title: 'Stay up to date',
-    // },
-  ];
-
+function Help() {
   return (
     <div className="docMainWrapper wrapper">
       <Container className="mainContainer documentContainer postContainer">
-        <div className="post">
+        <div className="container-fluid">
           <h1>Authors</h1>
           <p>
             This project is maintained by a group of researchers at the Center
@@ -57,11 +33,20 @@ function Help(props) {
           <p>
             Ask questions, and connect with other users and top contributors on{" "}
             <a href="https://stackoverflow.com">stackoverflow</a>. If your
-            question has not yet been answered please feel free to ask a new
-            question send us an email with the link to your question at{" "}
+            question has not yet been answered please ask the question on{" "}
+            <a href="https://stackoverflow.com">stackoverflow</a>
+            and send us an email with the link to your question at{" "}
             <a href="mailto:cbecomforttool@gmail.com">
               cbecomforttool@gmail.com
             </a>
+            .
+          </p>
+          <p>
+            <b>
+              Please do not ask us questions directly via email. We will only
+              respond to questions asked on{" "}
+              <a href="https://stackoverflow.com">stackoverflow</a>.
+            </b>
           </p>
           <h1>Issues and bugs</h1>
           <p>
@@ -77,14 +62,6 @@ function Help(props) {
               GitHub page.
             </a>{" "}
           </p>
-          <h1>For any other inquiries</h1>
-          <p>
-            You can contact us at{" "}
-            <a href="mailto:cbecomforttool@gmail.com">
-              cbecomforttool@gmail.com
-            </a>
-          </p>
-          <GridBlock contents={supportLinks} layout="threeColumn" />
         </div>
       </Container>
     </div>
