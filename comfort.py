90c553d99d37024c083608d7a7150b48a9e9761a
Federico <federicotartarini@gmail.com>
2022-09-09 10:07:39 +0800

    feat: added 404 page, bump version

--- comfort.py
+++ comfort.py
@@ -180,6 +180,11 @@ def en():
     return render_template("en.html")
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
 @app.route("/")
 def index():
     return render_template("ashrae.html")
